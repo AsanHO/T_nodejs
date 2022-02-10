03a2dbc1309fe25872a2ec130195a372ab2a809b
AsanHO <gorma00@naver.com>
2022-02-10 22:26:02 +0900

    morgan

--- src/server.js
+++ src/server.js
@@ -1,41 +1,25 @@
 import express from "express";
-import res from "express/lib/response";
+import morgan from "morgan";
 
 const PORT = 4000;
 
 const app = express();
 
-const testMiddleware = (req, res, next) => {
-    console.log(`${req.method} ${req.url}`);
-    next();
-};
-
-const privateMiddleware = (req, res, next) => {
-    const url = req.url;
-    if(url === "/private") {
-        return res.send("<h1>접근불가</h1>");
-    };
-    console.log(`${url}로 접근허용`);
-    next();
-};
+const logger = morgan("dev")
 
 const handlehome = (req, res) => {
-    return res.send("Hello world");
-};
+    console.log("home")
+    return res.send("home")
+}
 
 const handlelogin = (req, res) => {
-    return res.send("login here");
-};
+    console.log("login")
+    return res.send("login")
+}
 
-const privatepage = (req, res) => {
-    return res.send("welcome to my private lounge");
-};
-
-app.use(testMiddleware);
-app.use(privateMiddleware);
+app.use(logger)
 app.get("/", handlehome);
 app.get("/login", handlelogin);
-app.get("/private", privatepage);
 
 const handleListening = () => console.log(`✅ Server Listening on port http://localhost:${PORT} 🚀`);
 
