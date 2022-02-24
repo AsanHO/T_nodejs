25792db546fe805b755233c4cb39682df5205341
AsanHO <gorma00@naver.com>
2022-02-24 16:21:56 +0900

    User Create and Status Code

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,5 +1,34 @@
-//to globalRouter
-export const join = (req, res) => res.send("join");
+import User from "../models/User";
+//to rootRouter
+export const getJoin = (req, res) => res.render("join", { pageTitle: "join" });
+export const postJoin = async (req, res) => {
+  const { name, username, email, password, password2, location } = req.body;
+  const pageTitle = "join";
+  if (password !== password2) {
+    return res.status(400).render("join", {
+      pageTitle,
+      errorMessage: "Password confirmation does not match",
+    });
+  }
+  const exists = await User.exists({
+    $or: [{ username }, { email }],
+    //=$or: [{ username: req.body.username }, { email: req.body.email }],
+  });
+  if (exists) {
+    return res.status(400).render("join", {
+      pageTitle,
+      errorMessage: "This username/email is already taken.",
+    });
+  }
+  await User.create({
+    email,
+    username,
+    password,
+    name,
+    location,
+  });
+  return res.redirect("/login");
+};
 export const login = (req, res) => res.send("login");
 
 //to userRouter
--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -1,4 +1,3 @@
-import res from "express/lib/response";
 import Video from "../models/Video";
 
 //to globalRouter
@@ -18,14 +17,14 @@ export const watch = async (req, res) => {
   if (video) {
     return res.render("watch", { pageTitle: video.title, video: video });
   }
-  return res.render("404", { pageTitle: "Video not founded" });
+  return res.status(404).render("404", { pageTitle: "Video not founded" });
 };
 
 export const getEdit = async (req, res) => {
   const { id } = req.params;
   const video = await Video.findById(id);
   if (!video) {
-    return res.render("404", { pageTitle: "Video not founded" });
+    return res.status(404).render("404", { pageTitle: "Video not founded" });
   }
   return res.render("edit", { pageTitle: `editing ${video.title}`, video });
 };
@@ -35,7 +34,7 @@ export const postEdit = async (req, res) => {
   const { title, description, hashtags } = req.body;
   const video = await Video.exists({ _id: id });
   if (!video) {
-    return res.render("404", { pageTitle: "Video not founded" });
+    return res.status(404).render("404", { pageTitle: "Video not founded" });
   }
   await Video.findByIdAndUpdate(id, {
     title,
@@ -59,7 +58,7 @@ export const postUpload = async (req, res) => {
     });
     return res.redirect("/");
   } catch (error) {
-    return res.render("upload", {
+    return res.status(400).render("upload", {
       pageTitle: "Upload Video",
       errorMessage: error._message,
     });
