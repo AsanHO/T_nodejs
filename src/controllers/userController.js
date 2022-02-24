151b53d8f4daba2ef91a80a4a73ebad518736d55
AsanHO <gorma00@naver.com>
2022-02-24 22:24:21 +0900

    sessions and cookie

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,4 +1,6 @@
+import { render } from "express/lib/response";
 import User from "../models/User";
+import bcrypt from "bcrypt";
 //to rootRouter
 export const getJoin = (req, res) => res.render("join", { pageTitle: "join" });
 export const postJoin = async (req, res) => {
@@ -20,16 +22,51 @@ export const postJoin = async (req, res) => {
       errorMessage: "This username/email is already taken.",
     });
   }
-  await User.create({
-    email,
-    username,
-    password,
-    name,
-    location,
-  });
-  return res.redirect("/login");
+  try {
+    await User.create({
+      email,
+      username,
+      password,
+      name,
+      location,
+    });
+    return res.redirect("/login");
+  } catch (error) {
+    return (
+      res.status(400),
+      render("join", {
+        pageTitle,
+        errorMessage: error._message,
+      })
+    );
+  }
 };
-export const login = (req, res) => res.send("login");
+
+export const getLogin = (req, res) =>
+  res.render("login", { pageTitle: "Login" });
+export const postLogin = async (req, res) => {
+  const { username, password } = req.body;
+  const pageTitle = "login";
+  // 사용자가 입력한 username이 DB에도 있는지 확인
+  const user = await User.findOne({ username: username }); //=username하나만 써도 유효
+  if (!user) {
+    return res.status(400).render("login", {
+      pageTitle,
+      errorMessage: "존재하지 않는 유저이름입니다~~",
+    });
+  }
+  // 비밀번호 유효성 체크
+  const ok = await bcrypt.compare(password, user.password);
+  if (!ok) {
+    return res.status(400).render("login", {
+      pageTitle,
+      errorMessage: "비밀번호 틀렸쥬?",
+    });
+  }
+  req.session.loggedIn = true;
+  req.session.user = user;
+  return res.redirect("/");
+};
 
 //to userRouter
 export const see = (req, res) => res.send("see");
--- src/server.js
+++ src/server.js
@@ -1,5 +1,7 @@
 import express from "express";
+import session from "express-session";
 import morgan from "morgan";
+import { localsMiddleware } from "./middlewares";
 import rootRouter from "./routers/rootRouter";
 import userRouter from "./routers/userRouter";
 import videoRouter from "./routers/videoRouter";
@@ -14,6 +16,21 @@ app.set("view engine", "pug");
 app.set("views", process.cwd() + "/src/views");
 app.use(logger);
 app.use(express.urlencoded({ extended: true }));
+
+app.use(
+  session({
+    secret: "Hello!",
+    resave: true,
+    saveUninitialized: true,
+  })
+);
+app.use((req, res, next) => {
+  req.sessionStore.all((error, sessions) => {
+    console.log(sessions);
+    next();
+  });
+});
+app.use(localsMiddleware);
 app.use("/", rootRouter);
 app.use("/users", userRouter);
 app.use("/videos", videoRouter);
