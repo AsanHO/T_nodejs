f4cf6bb544ba4930ebd5241f3277d8d6a3f78a30
AsanHO <gorma00@naver.com>
2022-04-04 09:04:12 +0900

    comment

---
+++ src/client/js/commentSection.js
@@ -0,0 +1,78 @@
+import fetch from "node-fetch";
+
+const videoContainer = document.getElementById("videoContainer");
+const form = document.getElementById("commentForm");
+const textarea = form.querySelector("textarea");
+const deleteBtn = document.querySelectorAll(".deleteBtn");
+
+const addComment = (text, id) => {
+  const commentContainer = document.querySelector(".video__comments ul");
+  const commentList = document.createElement("li");
+  commentList.dataset.id = id;
+  commentList.classList.add("video__comment");
+  const icon = document.createElement("i");
+  const span = document.createElement("span");
+  const span2 = document.createElement("span");
+  icon.className = "fas fa-comment";
+  span.innerText = ` ${text}`;
+  span2.classList.add("deleteBtn");
+  span2.innerText = "❌";
+  commentList.appendChild(icon);
+  commentList.appendChild(span);
+  commentList.appendChild(span2);
+  commentContainer.prepend(commentList);
+  span2.addEventListener("click", handleDelete);
+};
+
+const deleteComment = (event) => {
+  const commentContainer = document.querySelector(".video__comments ul");
+  const commentList = event.target.parentNode;
+  commentContainer.removeChild(commentList);
+};
+
+const handleSubmit = async (event) => {
+  event.preventDefault();
+  const text = textarea.value;
+  const videoId = videoContainer.dataset.id;
+  if (text === "") {
+    return;
+  }
+  const response = await fetch(`/api/videos/${videoId}/comment`, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({ text }),
+  });
+  if (response.status === 201) {
+    const { newCommentId } = await response.json();
+    textarea.value = "";
+    addComment(text, newCommentId);
+  }
+};
+
+const handleDelete = async (event) => {
+  const commentList = event.target.parentNode;
+  const commentId = commentList.dataset.id;
+  const videoId = videoContainer.dataset.id;
+  const response = await fetch(`/api/comments/${commentId}/delete`, {
+    method: "DELETE",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      videoId,
+    }),
+  });
+  if (response.status === 201) {
+    deleteComment(event);
+  }
+  if (response.status === 403) {
+    alert("댓글 주인이 아닙니다.");
+  }
+};
+
+form.addEventListener("submit", handleSubmit);
+for (let i = 0; i < deleteBtn.length; i++) {
+  deleteBtn[i].addEventListener("click", handleDelete);
+}
--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -1,4 +1,5 @@
 import User from "../models/User";
+import Comment from "../models/Comment";
 import Video from "../models/Video";
 
 //to globalRouter
@@ -16,7 +17,7 @@ export const home = async (req, res) => {
 export const watch = async (req, res) => {
   const { id } = req.params;
   //ㄴ const id = req.params.id; 링크에서 뽑는것
-  const video = await Video.findById(id).populate("owner");
+  const video = await Video.findById(id).populate("owner").populate("comments");
   /*==% const video = await Video.findById(id);
   const owner = await User.findById(video.owner);*/
   if (!video) {
@@ -136,4 +137,51 @@ export const registerView = async (req, res) => {
   video.meta.views = video.meta.views + 1;
   await video.save();
   return res.sendStatus(200);
+};
+
+export const createComment = async (req, res) => {
+  const {
+    session: { user },
+    body: { text },
+    params: { id },
+  } = req;
+  const video = await Video.findById(id);
+  if (!video) {
+    return res.sendStatus(404);
+  }
+  const commentUser = await User.findById(user._id);
+  const comment = await Comment.create({
+    text,
+    owner: user._id,
+    video: id,
+  });
+  video.comments.push(comment._id);
+  commentUser.comments.push(comment._id);
+  commentUser.save();
+  video.save();
+  req.session.user = commentUser; //
+  return res.status(201).json({
+    newCommentId: comment._id,
+  });
+};
+
+export const deleteComment = async (req, res) => {
+  const {
+    params: { id },
+    body: { videoId },
+    session: { user },
+  } = req;
+  const video = await Video.findById(videoId);
+  const commentUser = await User.findById(user._id);
+  if (user.comments.indexOf(id) < 0) {
+    req.flash("info", "Not authorized");
+    return res.sendStatus(403);
+  }
+  commentUser.comments.splice(commentUser.comments.indexOf(id), 1);
+  video.comments.splice(video.comments.indexOf(id), 1);
+  await video.save();
+  await commentUser.save();
+  await Comment.findByIdAndDelete(id);
+
+  return res.sendStatus(201);
 };
